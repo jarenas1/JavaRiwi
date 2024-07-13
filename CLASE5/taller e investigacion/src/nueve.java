e6afaffaa76fb0c8dccddbd592a5008f9aeddee4
jarenas1 <juanjoarenas1218gmail.com>
2024-07-12 20:18:05 -0500

    correcciones

--- CLASE5/taller e investigacion/src/nueve.java
+++ CLASE5/taller e investigacion/src/nueve.java
@@ -5,32 +5,31 @@ import java.util.TreeMap;
 
 public class nueve {
     public static void main(String[] args) {
-        String Frase = "antony me va a poner cinco porque antony es un bien y antony antony carros carros casa casa casa dios";
-        String[] fraseArray = Frase.toLowerCase().split(" ");
+        String txt = "antony me va a poner cinco porque antony es un bien y antony antony carros carros casa casa casa dios";
+        String[] arreglo = txt.toLowerCase().split(" ");
 
-        int cuentas = 0;
+        int count = 0;
         Map<Integer, String> palabrasCantidad = new HashMap<Integer, String>();
 
 
-        for (int i = 0; i < fraseArray.length; i++) {
-            for (int j = 0; j < fraseArray.length; j++) {
-                if(fraseArray[i].equals(fraseArray[j])){
-                    cuentas++;
+        for (int i = 0; i < arreglo.length; i++) {
+            for (int j = 0; j < arreglo.length; j++) {
+                if(arreglo[i].equals(arreglo[j])){
+                    count++;
                 }
             }
-            if(cuentas > 1){
-                palabrasCantidad.put(cuentas, fraseArray[i]);
-                cuentas = 0;
+            if(count > 1){
+                palabrasCantidad.put(count, arreglo[i]);
+                count = 0;
             }
         }
-        //palabrasCantidad.forEach((k, v) -> System.out.println(v + ": " + k));
-        //Para ordenar las ocurrencias
-        Map<Integer, String> palabrasOrden = new TreeMap<Integer, String>(palabrasCantidad);
-        Iterator it = palabrasOrden.keySet().iterator();
+
+        Map<Integer, String> orden = new TreeMap<Integer, String>(palabrasCantidad);
+        Iterator it = orden.keySet().iterator();
 
         while (it.hasNext()) {
             int key = (int)it.next();
-            System.out.println("Palabra: " + palabrasOrden.get(key) + ". Cantidad " + key);
+            System.out.println("palabras : " +orden.get(key) + ". Value " + key);
     }
 }
 }
